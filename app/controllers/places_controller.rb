65c34c1739af17b53fcc2e618896f9be5012c818
shanekrusen <shanekrusen@gmail.com>
2016-05-10 01:10:50 -0400

    added pagination to index

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,7 @@
 class PlacesController < ApplicationController
   
   def index
-    @places = Place.all
+    @places = Place.paginate(:page => params[:page], :per_page => 1)
+    @background_colors = ["#df494e", "#68a5b7", "#8f978e", "#d8b6ad", "#677b94", "#677b94F", "#66be98", "#fcd06d", "#f8a427", "#d42b14"]
   end
 end
