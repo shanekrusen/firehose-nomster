e7d29208e2175a9d8d602a0598f1d4575f63e968
Shane Krusen <shanekrusen@gmail.com>
2016-05-23 00:06:05 -0400

    Added dropdown rating to new comment form, css changes to new comment form

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,17 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
+  
+  RATINGS = {
+    '0/5' => '0_5',
+    '1/5' => '1_5',
+    '2/5' => '2_5',
+    '3/5' => '3_5',
+    '4/5' => '4_5',
+    '5/5' => '5_5'
+  }
+  
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
 end
